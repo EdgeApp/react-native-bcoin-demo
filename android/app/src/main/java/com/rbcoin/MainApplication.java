bdd02ed47d88814aeee56ba9cd98915771096266
Stanislav Synko <safitudo@gmail.com>
2017-05-10 17:21:50 -0700

    buffer => working with browser debugging

--- android/app/src/main/java/com/rbcoin/MainApplication.java
+++ android/app/src/main/java/com/rbcoin/MainApplication.java
@@ -3,6 +3,8 @@ package com.rbcoin;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.bitgo.randombytes.RandomBytesPackage;
+import com.peel.react.TcpSocketsModule;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RandomBytesPackage(),
+            new TcpSocketsModule()
       );
     }
   };
